bcd818d676d684421cc790a860e6a95a0f3af4d7
HenskiS <59155518+HenskiS@users.noreply.github.com>
2025-01-23 12:55:39 -0800

    Update fix_migrations_2024_typo.js

--- server/fix_migrations_2024_typo.js
+++ server/fix_migrations_2024_typo.js
@@ -1,3 +1,3 @@
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('../database.sqlite');
+const db = new sqlite3.Database('database.sqlite');
 db.run("UPDATE SequelizeMeta SET name = REPLACE(name, '2024', '2025') WHERE name LIKE '2024%'");
